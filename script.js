899ff9da114386783922b86b5a9aaca5227256cb
CodeEryk <ericjeb3@gmail.com>
2020-05-28 15:27:24 +0100

    Final Version

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(respon
 
 function addToSite(lists){
 
-    let currentList = document.getElementById('list');
+    let liveList = document.getElementById('list');
     lists.forEach(list => {
     let x = document.createElement('li');
     x.innerText= list;
-    currentList.appendChild(x); 
+    liveList.appendChild(x); 
     })
 }
 
 function saveContent(){
-    let actualContent = document.getElementById('contentValue')[0].value;
+    let actualContent = document.getElementById('mainForm')[0].value;
 
     fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall', {
     method: 'POST',
@@ -21,12 +21,10 @@ function saveContent(){
         console.log("Ok!")
     }   else {console.log("Oh no!")}
     })
-}
 
-fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json()).then(data => console.log(data))
-
-if (localStorage.getItem('test') == "key") {
-  console.log("It's there!")
-} else {
-  localStorage.setItem('test', 'key')
+    let liveList = document.getElementById('list').lastElementChild;
+    let newContent = document.createElement('li');
+    newContent.innerText= actualContent;
+    liveList.appendChild(newContent);
+    return false;
 }
